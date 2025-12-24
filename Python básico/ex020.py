b56541823a4d48df87b66ad8a618905a49cb2261
rafaelrdacosta <rafaelrdacosta@gmail.com>
2025-12-24 15:50:09 -0400

    Estudo 24 dez 25

---
+++ Python básico/ex020.py
@@ -0,0 +1,17 @@
+# Tipos numéricos
+inteiro = 10
+flutuante = 5.5
+
+# Operadores numéricos
+soma = inteiro + flutuante
+produto = inteiro * flutuante
+
+# Tipo booleano
+verdadeiro = True
+falso = False
+
+#Exibindo resultados
+print('Soma:', soma)
+print('Produto:', produto)
+print('Verdadeiro:', verdadeiro)
+print('Falso:', falso)
---
+++ Python básico/ex022.py
@@ -0,0 +1,4 @@
+# Precedência entre operadores
+
+resultado = 2 + 3 - 4 ** 2 + 5 / 2 - 5 // 2
+print('Resultado: ', resultado)
