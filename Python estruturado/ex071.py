26bcade0347556d32b5fce090870cd980fd86361
rafaelrdacosta <rafaelrdacosta@gmail.com>
2026-02-09 20:31:39 -0300

    Estudos 9 FEV 26

---
+++ Python estruturado/ex071.py
@@ -0,0 +1,33 @@
+# Torre de Hanói
+
+def mover_disco(origem, destino):
+    disco = origem.pop()
+    destino.append(disco)
+
+def imprimir_torres(torre_A, torre_B, torre_C):
+    print('A:', torre_A)
+    print('B:', torre_B)
+    print('C:', torre_C)
+    print()
+
+def torres_de_hanoi_recursivo(num_discos, origem, destino, auxiliar):
+    if num_discos == 1:
+        mover_disco(origem, destino)
+        imprimir_torres(torre_A, torre_B, torre_C)
+    else:
+        torres_de_hanoi_recursivo(num_discos - 1, origem, auxiliar, destino)
+        mover_disco(origem, destino)
+        imprimir_torres(torre_A, torre_B, torre_C)
+        torres_de_hanoi_recursivo(num_discos - 1, auxiliar, destino, origem)
+
+# resolvendo o problema recursivamente
+num_disco = 3
+
+# inicializando as torres e os discos
+torre_A = list(range(num_disco, 0, -1))
+torre_B = []
+torre_C = []
+
+# mostrando o estado inicial
+imprimir_torres(torre_A, torre_B, torre_C)
+torres_de_hanoi_recursivo(num_disco, torre_A, torre_C, torre_B)
